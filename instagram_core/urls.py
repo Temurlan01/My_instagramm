1d802bcc7119b09f0ae07a3dd14e365305400f10
Temurlan01 <erkinbaevtemurlan@gmail.com>
2024-11-29 13:42:18 +0600

    "wen"

--- instagram_core/urls.py
+++ instagram_core/urls.py
@@ -2,10 +2,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from users.views import UserRegistrationView, MakeUserRegistrationView
+from users.views import UserRegistrationView, MakeUserRegistrationView, LoginListView, MakeLoginView, HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('registrations/', UserRegistrationView.as_view(), name='registrations-url'),
-    path('make-registration', MakeUserRegistrationView.as_view(), name='make-registration-url')
+    path('make-registration/', MakeUserRegistrationView.as_view(), name='make-registration-url'),
+    path('login/', LoginListView.as_view(), name='login-url'),
+    path('make-login/', MakeLoginView.as_view(), name='make-login-url'),
+
+    path('home/', HomeView.as_view())
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from urllib import request
+from django.contrib.auth import login
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
+
+from users.models import CustomUser
 
 
 class UserRegistrationView(TemplateView):
@@ -9,8 +12,41 @@ class UserRegistrationView(TemplateView):
 
 
 class MakeUserRegistrationView(View):
-    data = request.POST
 
     def post(self, request, *args, **kwargs):
-
+        data = request.POST
+        print('это то что ', data)
+        first_name = data['first_name'],
+        last_name = data['last_name'],
+        username = data['username'],
+        password = data['password']
+        user = CustomUser.objects.create_user(
+            nickname=username,password=password,
+            first_name=first_name, last_name=last_name,
+        )
         return render(request, 'login.html')
+
+
+class LoginListView(TemplateView):
+    template_name = 'login.html'
+
+
+class MakeLoginView(View):
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        nickname = data['username'],
+        password = data['password']
+
+        user = CustomUser.objects.get(nickname=nickname)
+        print('Пользователь', user)
+        correct = user.check_password(password)
+        print('коррект равен', correct)
+        if correct == True:
+            login(request, user)
+            return render(request, 'home.html', context={'logged_in': True})
+        else:
+            return render(request, 'login.html', context={'logged_in': False})
+
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
